af08a778f69e82ad3942f04463fa9b0a41fc56a9
Jefferson Brandao <jeffersonbraster@gmail.com>
2020-01-11 10:29:19 -0300

    criando projeto, hibernate util

---
+++ src/crud/VariavelConexaoUtil.java
@@ -0,0 +1,12 @@
+package crud;
+
+/*
+ * Nome do caminho do JNDI datasource tomcat
+ * 
+ */
+
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+}
