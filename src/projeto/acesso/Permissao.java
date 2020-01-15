7bbc5ac783062552033d0554aefb1affd25cd030
Jefferson Brandao <jeffersonbraster@gmail.com>
2020-01-14 21:31:17 -0300

    enum de permissao de acessos

---
+++ src/projeto/acesso/Permissao.java
@@ -0,0 +1,51 @@
+package projeto.acesso;
+
+public enum Permissao {
+	
+	ADMIN("ADMIN", "Administrador"),
+	USER("USER", "Usuário Padrão"),
+	CADASTRO_ACESSAR("CADASTRO_ACESSAR", "Cadastro - Acessar"),
+	FINANCEIRO_ACESSAR("FINANCEIRO_ACESSAR", "Financeiro - Acessar"),
+	MENSAGEM_ACESSAR("MENSAGEM_ACESSAR", "Mensagem recebida - Acessar"),
+	
+	BAIRRO_ACESSAR("BAIRRO_ACESSAR", "Bairro - Acessar"),
+	BAIRRO_NOVO("BAIRRO_NOVO", "Bairro - Novo"),
+	BAIRRO_EDITAR("BAIRRO_EDITAR", "Bairro - Editar"),
+	BAIRRO_EXCLUIR("BAIRRO_EXCLUIR", "Bairro - Excluir");
+	
+	
+	private String valor = "";
+	private String descricao = "";
+	
+	
+	private Permissao(String name, String descricao) {
+		this.valor = name;
+		this.descricao = descricao;
+	}
+	
+	private Permissao() {
+		
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	@Override
+	public String toString() {		
+		return getValor();
+	}
+
+}
