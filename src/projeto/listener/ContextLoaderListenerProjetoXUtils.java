1b829307f9b6435c26828d08a1340212d7ec6934
Jefferson Brandao <jeffersonbraster@gmail.com>
2020-01-21 21:08:44 -0300

    Criando o ContextLoaderListener do spring

---
+++ src/projeto/listener/ContextLoaderListenerProjetoXUtils.java
@@ -0,0 +1,29 @@
+package projeto.listener;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ApplicationScoped;
+
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+@ApplicationScoped
+public class ContextLoaderListenerProjetoXUtils extends ContextLoader implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static WebApplicationContext getWac() {
+		return WebApplicationContextUtils.getWebApplicationContext(getCurrentWebApplicationContext().getServletContext());
+	}
+	
+	public static Object getBean(String idNomeBean) {
+		return getWac().getBean(idNomeBean);
+		
+	}
+	
+	public static Object getBean(Class<?> classe) {
+		return getWac().getBean(classe);
+	}
+
+}
