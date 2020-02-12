e1cfc11c618ac1831be145040dee16578f02c2d6
Jefferson Brandao <jeffersonbraster@gmail.com>
2020-02-12 20:52:53 -0300

    Iniciando a criação do cadastro de cidades

--- src/projeto/bean/view/CidadeBeanView.java
+++ src/projeto/bean/view/CidadeBeanView.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 
 import projeto.bean.geral.BeanManagedViewAbstract;
 import projeto.geral.controller.CidadeController;
+import projeto.model.classes.Cidade;
 
 @Controller
 @Scope(value = "session")
@@ -18,7 +19,31 @@ public class CidadeBeanView extends BeanManagedViewAbstract {
 
 	private static final long serialVersionUID = 1L;
 	
+	private Cidade objetoSelecionado = new Cidade();
+	
 	@Autowired
 	private CidadeController cidadeController;
+	
+	@Override
+	public String save() throws Exception {
+		System.out.println(objetoSelecionado.getCid_descricao());
+		return "";
+	}
+	
+	
+	
+	
+
+	public Cidade getObjetoSelecionado() {
+		return objetoSelecionado;
+	}
+
+	public void setObjetoSelecionado(Cidade objetoSelecionado) {
+		this.objetoSelecionado = objetoSelecionado;
+	}
+	
+	
+	
+	
 
 }
