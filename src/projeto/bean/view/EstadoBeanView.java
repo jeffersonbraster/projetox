acbc935b8fdac8e1b599cfac1de48000abb60fbf
Jefferson Brandao <jeffersonbraster@gmail.com>
2020-02-12 21:32:16 -0300

    Criando tela de cidades - converter de estados

---
+++ src/projeto/bean/view/EstadoBeanView.java
@@ -0,0 +1,31 @@
+package projeto.bean.view;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import projeto.bean.geral.BeanManagedViewAbstract;
+import projeto.geral.controller.EstadoController;
+
+@Controller
+@Scope(value = "session")
+@ManagedBean(name = "estadoBeanView")
+public class EstadoBeanView extends BeanManagedViewAbstract {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Autowired
+	private EstadoController estadoController;
+	
+	
+	public List<SelectItem> getEstados() throws Exception {
+		return estadoController.getListEstado();
+	}
+
+}
